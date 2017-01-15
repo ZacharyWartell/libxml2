cb71d24c69025b488eb15e3bac7d2345d47df1c4
Zachary Wartell <zwartell@uncc.edu>
2017-01-14 23:46:16 -0500

    -progress...

--- UNCC_ZJW-build/msvs-2010-x64.bat
+++ UNCC_ZJW-build/msvs-2010-x64.bat
@@ -5,7 +5,7 @@ echo off
 
 pushd ..\win32
 
-set TPL_DIR=.
+set TPL_DIR=..\..
 set INSTALL_DIR=%TPL_DIR%\install\v100-x64\
 set INSTALL_BIN_DIR=%INSTALL_DIR%bin
 set INSTALL_LIB_DIR=%INSTALL_DIR%lib
@@ -13,9 +13,23 @@ set INSTALL_INC_DIR=%INSTALL_DIR%include
 
 REM IF NOT EXIST "%INSTALL_INC_DIR%\iconv" mkdir "%INSTALL_INC_DIR%\iconv"
 
-cscript configure.js compiler=msvc cruntime=/MDd debug=no bindir=%INSTALL_BIN_DIR% libdir=%INSTALL_LIB_DIR% include=%INSTALL_INC_DIR% lib=%INSTALL_LIB_DIR% sodir=%INSTALL_BIN_DIR%
+REM dir %INSTALL_DIR%
+REM dir %INSTALL_INC_DIR%\iconv
+dir %INSTALL_LIB_DIR%
 
-REM "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" amd64
-cmd /C "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" amd64 & nmake /f Makefile.msvc
+REM popd
+REM exit /b
+
+cscript configure.js compiler=msvc cruntime=/MDd debug=no bindir=%INSTALL_BIN_DIR% libdir=%INSTALL_LIB_DIR% include="%INSTALL_INC_DIR%\iconv" lib=%INSTALL_LIB_DIR% sodir=%INSTALL_BIN_DIR%
+
+REM \todo generalize this line 
+REM other architectures?
+
+pushd %VS100COMNTOOLS%
+call ..\..\VC\vcvarsall.bat" amd64
+popd
+
+nmake /f Makefile.msvc
+nmake /f Makefile.msvc install
 
 popd
