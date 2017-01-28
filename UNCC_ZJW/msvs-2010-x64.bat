502a77dbd213b8f3d4ad98359d39f2da23b8fe3c
Zachary Wartell <zwartell@uncc.edu>
2017-01-28 00:21:33 -0500

    -refactoring using gntb

--- UNCC_ZJW/msvs-2010-x64.bat
+++ UNCC_ZJW/msvs-2010-x64.bat
@@ -1,43 +1,58 @@
+echo off
 REM \author Zachary Wartell <zwartell@uncc.edu>
 REM 
 REM Run this script from libxml2\UNCC_ZJW
 REM
+REM See USAGE:
+REM 
 REM This script is an attempt to automate the instructions in libxml2\win32\Readme.txt
-REM The compiler settings and directory paths are based my needs for compiling the suite
+REM Further, the compiler settings and directory paths are based my needs for compiling the suite
 REM of open source code described here: 
 REM
 REM     https://cci-git.uncc.edu/UNCC_Graphics_Third_Party_Libraries/AMVR-Third_Party_Libraries.git 
+REM
 
-echo off
+REM 
+REM USAGE
+REM
+if "%1"=="/?" (
+	echo. 
+	echo USAGE:
+	echo. 
+	echo msvs_compile.bat MSVS_BITS MSVS_VERSION
+	echo. 
+	echo MSVS_BITS = 32 or 64 
+	echo      determines whether compilation result is for 32 or 64 architecture
+	echo. 
+	echo MSVS_VERSION = 2010 or 2015
+	echo      determines what version of MSVS is used to compile boost. 
+	echo. 
+	exit /b
+)
 
 REM \todo how to make this more flexible, will this generate code the fails on intel64 machines?
-
-REM set MSVS_ARCH=amd64
-
-set MSVS_ARCH=x86_amd64
-set MSVS_ARCH=amd64
-
-REM these choices all generate xmllint.exe and other executables that crashed immediately
-REM set MSVS_ARCH=amd64
-REM set MSVS_ARCH=x86_amd64
 
 pushd GumNTape_Build
 set GUMNTAPE_BUILD=%CD%
 popd
 
+REM setup MSVS compiler tools
+call %GUMNTAPE_BUILD%\common\msvs_tools_setup.bat 64 2010
+
 pushd ..\win32
 
 set TPL_DIR=..\..
-set INSTALL_DIR=%TPL_DIR%\install\MSVS_2010\x64\
-set INSTALL_BIN_DIR=%INSTALL_DIR%bin
-set INSTALL_LIB_DIR=%INSTALL_DIR%lib
-set INSTALL_INC_DIR=%INSTALL_DIR%include
+call "%GUMNTAPE_BUILD%\common\v1\msvs_set_install_paths.bat" %TPL_DIR% 64 2010
+
+REM set DEBUG1=1
+if DEFINED DEBUG1 (
+    echo INSTALL_DIR %INSTALL_DIR%
+    echo INSTALL_BIN_DIR %INSTALL_BIN_DIR%
+    echo INSTALL_LIB_DIR %INSTALL_LIB_DIR%
+    echo INSTALL_INC_DIR %INSTALL_INC_DIR%
+)
 
 REM IF NOT EXIST "%INSTALL_INC_DIR%\iconv" mkdir "%INSTALL_INC_DIR%\iconv"
-
-REM dir %INSTALL_DIR%
-REM dir %INSTALL_INC_DIR%\iconv
-dir %INSTALL_LIB_DIR%
 
 REM popd
 REM exit /b
@@ -62,15 +77,11 @@ REM
 REM /MDd - generates xmllint.exe that crashes
 REM /MD  - generates xmllint.exe that crashes
 REM /MTd - ditto
+REM
+
 cscript configure.js compiler=msvc cruntime=/MDd debug=yes bindir=%INSTALL_BIN_DIR% libdir=%INSTALL_LIB_DIR% incdir=%INSTALL_INC_DIR% include="%INSTALL_INC_DIR%\iconv" lib=%INSTALL_LIB_DIR% sodir=%INSTALL_BIN_DIR%
 
-REM \todo generalize this line 
-REM other architectures?
-
-echo on
-call %GUMNTAPE_BUILD%\common\msvs_tools_setup.bat 64 2010
-
-goto exit_popd
+REM goto exit_popd
 
 echo Make Clean
 nmake /f Makefile.msvc clean 
